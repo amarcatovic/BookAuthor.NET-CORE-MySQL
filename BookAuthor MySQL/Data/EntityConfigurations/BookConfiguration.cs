c9eca9ea5734b3e1d0ae0d1de84687369bc812e8
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 17:11:13 +0200

    add config for book class

---
+++ BookAuthor MySQL/Data/EntityConfigurations/BookConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using BookAuthor_MySQL.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.EntityConfigurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.HasIndex(b => b.Name);
+            builder.HasIndex(b => b.Published);
+
+            builder.Property(b => b.Name)
+                .IsRequired();
+
+            builder.Property(b => b.Published)
+                .IsRequired();
+
+            builder.Property(b => b.Description)
+                .IsRequired();
+        }
+    }
+}
