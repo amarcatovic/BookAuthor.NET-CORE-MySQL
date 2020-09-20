547e2b04c4ad331be85fbec222f7684f411b963f
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 17:30:29 +0200

    Add Interfaces

---
+++ BookAuthor MySQL/Data/Repos/Interfaces/IBookRepo.cs
@@ -0,0 +1,15 @@
+﻿using BookAuthor_MySQL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.Repos.Interfaces
+{
+    public interface IBookRepo
+    {
+        Task<Book> GetBookById(int id);
+        Task<IEnumerable<Book>> GetBooks();
+        Task CreateBook(Book book);
+    }
+}
---
+++ BookAuthor MySQL/Data/Repos/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.Repos.Interfaces
+{
+    public interface IUnitOfWork :IDisposable
+    {
+        IBookRepo Books { get; }
+        IAuthorRepo Authors { get; }
+        
+        Task Done();
+    }
+}
