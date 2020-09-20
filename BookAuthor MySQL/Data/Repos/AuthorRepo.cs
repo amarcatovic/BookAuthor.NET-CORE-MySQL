94466ede81f5fa894a9bac4ae44f3013cad4425b
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 17:38:38 +0200

    Implement repo from interface

---
+++ BookAuthor MySQL/Data/Repos/AuthorRepo.cs
@@ -0,0 +1,45 @@
+﻿using BookAuthor_MySQL.Data.Models;
+using BookAuthor_MySQL.Data.Repos.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.Repos
+{
+    public class AuthorRepo : IAuthorRepo
+    {
+        private readonly DataContext _context;
+
+        public AuthorRepo(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAuthor(Author author)
+        {
+            await _context.Authors.AddAsync(author);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Author> GetAuthorById(int id)
+        {
+            var authorFromDb = await _context.Authors
+                .Include(a => a.AuthorBooks)
+                .ThenInclude(ab => ab.Book)
+                .SingleOrDefaultAsync(a => a.Id == id);
+
+            return authorFromDb;
+        }
+
+        public async Task<IEnumerable<Author>> GetAuthors()
+        {
+            var authorsFromDb = await _context.Authors
+                .Include(a => a.AuthorBooks)
+                .ThenInclude(ab => ab.Book)
+                .ToListAsync();
+
+            return authorsFromDb;
+        }
+    }
+}
