232eacf4c54e793ea9d6aef0617d9e492cd0c280
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 17:51:02 +0200

    Add mapping profiles

---
+++ BookAuthor MySQL/Data/Mapping Profiles/AuthorProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BookAuthor_MySQL.Data.Dtos;
+using BookAuthor_MySQL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.Mapping_Profiles
+{
+    public class AuthorProfiles: Profile
+    {
+        public AuthorProfiles()
+        {
+            CreateMap<Author, AuthorReadDto>()
+                .ForMember(dest => dest.Books, opt => opt.MapFrom(src => 
+                    src.AuthorBooks
+                        .Select(ab => ab.Book)));
+
+            CreateMap<Author, AuthorPatchDto>().ReverseMap();
+            CreateMap<AuthorCreateDto, Author>();
+        }
+    }
+}
---
+++ BookAuthor MySQL/Data/Mapping Profiles/BookProfiles.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BookAuthor_MySQL.Data.Dtos;
+using BookAuthor_MySQL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.Mapping_Profiles
+{
+    public class BookProfiles : Profile
+    {
+        public BookProfiles()
+        {
+            CreateMap<Book, BookReadDto>()
+                .ForMember(dest => dest.Authors, opt =>
+                    opt.MapFrom(src => src.BookAuthors
+                        .Select(ba => ba.Author)));
+
+            CreateMap<Book, BookPatchDto>().ReverseMap();
+            CreateMap<BookCreateDto, Book>();
+        }
+    }
+}
