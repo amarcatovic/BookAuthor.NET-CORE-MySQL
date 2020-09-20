20ac251bb6f6a0bffc19e798bb757d0fb496ab2b
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 17:12:50 +0200

    Apply configurations

--- BookAuthor MySQL/Data/DataContext.cs
+++ BookAuthor MySQL/Data/DataContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookAuthor_MySQL.Data.EntityConfigurations;
+using BookAuthor_MySQL.Data.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +10,18 @@ namespace BookAuthor_MySQL.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<AuthorBook> AuthorBook { get; set; }
+
         public DataContext(DbContextOptions<DataContext> opt): base(opt){}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new AuthorBookConfiguration());
+
             base.OnModelCreating(modelBuilder);
         }
     }
