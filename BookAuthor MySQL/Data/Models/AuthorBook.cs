99d15f8d3cff3cbf824ee8a8ce932a46a838cca5
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 17:04:06 +0200

    Add Join class model

---
+++ BookAuthor MySQL/Data/Models/AuthorBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.Models
+{
+    public class AuthorBook
+    {
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+        public Book Book { get; set; }
+        public int BookId { get; set; }
+    }
+}
