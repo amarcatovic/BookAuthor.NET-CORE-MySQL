1ddaaa8c0ced862f7cf0a6c3701fcbac8dcb8c26
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 17:03:52 +0200

    Add Book Model

---
+++ BookAuthor MySQL/Data/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Published { get; set; }
+        public ICollection<AuthorBook> BookAuthors { get; set; }
+    }
+}
