23d7a865e354883f092ef5c5f978fcdf9604b035
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 16:54:34 +0200

    Add Data Context class

---
+++ BookAuthor MySQL/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opt): base(opt){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
