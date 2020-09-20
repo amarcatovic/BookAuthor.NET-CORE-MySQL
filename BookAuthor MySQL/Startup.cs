3c3e148a22291ac3399d7d49c8a5a607c0352b7d
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 16:59:17 +0200

    use mysql config

--- BookAuthor MySQL/Startup.cs
+++ BookAuthor MySQL/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookAuthor_MySQL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,9 @@ namespace BookAuthor_MySQL
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<DataContext>(opt => 
+                opt.UseMySql(Configuration.GetConnectionString("DeveloperConnection")));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
