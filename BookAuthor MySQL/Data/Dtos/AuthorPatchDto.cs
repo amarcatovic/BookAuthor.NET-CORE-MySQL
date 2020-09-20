47881167f24fcf333d1d111c6ed052c3b1ad310b
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 18:21:10 +0200

    Fix Typo

--- BookAuthor MySQL/Data/Dtos/AuthorPatchDto.cs
+++ BookAuthor MySQL/Data/Dtos/AuthorPatchDto.cs
@@ -7,7 +7,6 @@ namespace BookAuthor_MySQL.Data.Dtos
 {
     public class AuthorPatchDto
     {
-        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DOB { get; set; }
