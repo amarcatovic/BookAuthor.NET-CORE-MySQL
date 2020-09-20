dcfd3031eed1d243f0422685edb0aeb22f7fbe95
Amar Ćatović <amar_catovic@hotmail.com>
2020-09-20 17:10:55 +0200

    Add config for join class

---
+++ BookAuthor MySQL/Data/EntityConfigurations/AuthorBookConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using BookAuthor_MySQL.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookAuthor_MySQL.Data.EntityConfigurations
+{
+    public class AuthorBookConfiguration : IEntityTypeConfiguration<AuthorBook>
+    {
+        public void Configure(EntityTypeBuilder<AuthorBook> builder)
+        {
+            builder.HasKey(ab => new { ab.BookId, ab.AuthorId });
+
+            builder.HasOne(ab => ab.Author)
+                .WithMany(a => a.AuthorBooks)
+                .HasForeignKey(ab => ab.AuthorId);
+
+            builder.HasOne(ab => ab.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ab => ab.BookId);
+        }
+    }
+}
